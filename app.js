17597adcb576f9f4f64d63febe1d4ac5f491c97d
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2023-11-03 15:25:36 +0300

    local logs removed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-app.use(require('./middleware/morgan'));
+//app.use(require('./middleware/morgan'));
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
