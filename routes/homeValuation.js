000ca0ed22db24dd102a4fab33b03fd96715c3e7
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2023-12-08 18:32:44 +0300

    Home Validation Post

--- routes/homeValuation.js
+++ routes/homeValuation.js
@@ -2,14 +2,21 @@ const express = require('express');
 const { create } = require('../models/valuationData.js')
 const router = express.Router();
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.render('home-valuation');
 })
 
-router.post('/collect',  (req, res)=>{
+router.post('/appointment', (req, res) => {
+    const step = req.query.step;
+
+
+
+})
+
+router.post('/collect', (req, res) => {
     const data = req.body;
-    create(data, (err, data)=>{
-        if(err){
+    create(data, (err, data) => {
+        if (err) {
             res.status(400).render()
         }
     })
